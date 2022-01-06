73ea5b150c711a2f5fb7e95f457ed36602223da9
Rinor Rama <rinor.rama95@gmail.com>
2022-01-06 16:02:41 +0100

    splitted app into components

---
+++ src/components/Emails/Emails.jsx
@@ -0,0 +1,21 @@
+function Emails(props) {
+	return (
+		<main className="emails">
+			<ul>
+				{props.filteredEmails.map((email, index) => (
+					<li key={index} className={`email ${email.read ? "read" : "unread"}`}>
+						<div className="select">
+							<input className="select-checkbox" type="checkbox" checked={email.read} onChange={() => props.toggleRead(email)} />
+						</div>
+						<div className="star">
+							<input className="star-checkbox" type="checkbox" checked={email.starred} onChange={() => props.toggleStar(email)} />
+						</div>
+						<div className="sender">{email.sender}</div>
+						<div className="title">{email.title}</div>
+					</li>
+				))}
+			</ul>
+		</main>
+	);
+}
+export default Emails;
